2bc2c6b74c284e311952c8116c74cbb5adddcb06
zzmx-sudo <972167377@qq.com>
2023-02-26 19:02:31 +0800

    添加request插件

--- main.js
+++ main.js
@@ -2,6 +2,23 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+// 注册全局$http
+uni.$http = $http
+
+// 配置请求拦截器
+// 网络请求前拦截器，显示loading效果
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中'
+	})
+}
+// 网络请求后拦截器，关闭loading效果
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
