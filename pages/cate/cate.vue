aa708dd7e01db99f699dad990dbf65c3dcf4f236
zzmx-sudo <972167377@qq.com>
2023-03-01 21:02:13 +0800

    分类页面完成

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -13,7 +13,8 @@
 				<view class="right-scroll-view-item" v-for="(item2, i2) in childList" :key="i2">
 					<view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
 					<view class="cate-lv3-list">
-						<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3">
+						<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3"
+							@click="gotoGoodsList(item3)">
 							<image :src="item3.cat_icon.replace('dev', 'web')"></image>
 							<text>{{item3.cat_name}}</text>
 						</view>
@@ -61,6 +62,11 @@
 				this.childList = this.cateList[id].children
 				// scroll-top的值有变化才会滚动，因此让其在0和1之间切换
 				this.scrollTop = this.scrollTop === 0 ? 1 : 0
+			},
+			gotoGoodsList(item) {
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?cid=' + item.cat_id
+				})
 			}
 		}
 	}
