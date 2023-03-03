28ebd511e73ccca43245c8ef5bc47484896ef5ef
zzmx-sudo <972167377@qq.com>
2023-03-03 23:33:07 +0800

    完善商品组件

---
+++ components/goods-item/goods-item.vue
@@ -0,0 +1,78 @@
+<template>
+	<view>
+		<view class="goods-item">
+			<!-- 左侧的盒子 -->
+			<view class="goods-item-left">
+				<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+			</view>
+			<!-- 右侧的盒子 -->
+			<view class="goods-item-right">
+				<!-- 商品名称 -->
+				<view class="goods-name">{{goods.goods_name}}</view>
+				<!-- 商品信息 -->
+				<view class="goods-info-box">
+					<view class="goods-price">￥{{goods.goods_price | toFixed}}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "goods-item",
+		data() {
+			return {
+				// 默认图片地址
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		},
+		props: {
+			goods: {
+				type: Object,
+				default: {}
+			}
+		},
+		filters: {
+			toFixed(price) {
+				return Number(price).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+
+		.goods-item-left {
+			margin-right: 7px;
+
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+			}
+
+			.goods-info-box {
+				.goods-price {
+					color: #C00000;
+					font-size: 16px;
+					font-weight: 600;
+				}
+			}
+		}
+	}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -2,21 +2,7 @@
 	<view>
 		<view class="goods-list">
 			<block v-for="(goods, i) in goodsList" :key="i">
-				<view class="goods-item">
-					<!-- 左侧的盒子 -->
-					<view class="goods-item-left">
-						<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
-					</view>
-					<!-- 右侧的盒子 -->
-					<view class="goods-item-right">
-						<!-- 商品名称 -->
-						<view class="goods-name">{{goods.goods_name}}</view>
-						<!-- 商品信息 -->
-						<view class="goods-info-box">
-							<view class="goods-price">￥{{goods.goods_price.toFixed(2)}}</view>
-						</view>
-					</view>
-				</view>
+				<goods-item :goods="goods"></goods-item>
 			</block>
 		</view>
 	</view>
@@ -40,8 +26,6 @@
 				goodsList: [],
 				// 商品总数
 				total: 0,
-				// 默认图片地址
-				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
 			};
 		},
 		onLoad(options) {
@@ -68,37 +52,5 @@
 </script>
 
 <style lang="scss">
-	.goods-item {
-		display: flex;
-		padding: 10px 5px;
-		border-bottom: 1px solid #f0f0f0;
 
-		.goods-item-left {
-			margin-right: 7px;
-
-			.goods-pic {
-				width: 100px;
-				height: 100px;
-				display: block;
-			}
-		}
-
-		.goods-item-right {
-			display: flex;
-			flex-direction: column;
-			justify-content: space-between;
-
-			.goods-name {
-				font-size: 13px;
-			}
-
-			.goods-info-box {
-				.goods-price {
-					color: #C00000;
-					font-size: 16px;
-					font-weight: 600;
-				}
-			}
-		}
-	}
 </style>
